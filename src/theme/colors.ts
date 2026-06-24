ad3bb7549de22de0437d04956c199e5750fbdac1
Jesus <jesusrolandomc@gmail.com>
2026-06-24 16:12:55 -0700

    style: se agrega colors.ts

---
+++ src/theme/colors.ts
@@ -0,0 +1,9 @@
+// src/theme/colors.ts
+export const colors = {
+  background: '#0F1115',
+  card: '#15181F',
+  primary: '#2ECC71',
+  text: '#FFF',
+  textMuted: '#A0A0A0',
+  border: '#2A2D35',
+};
